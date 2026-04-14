462a37913593e797224a1893abebbb14ce6a3e57
Nicolas Couvert <nicolas.couvert@obeosoft.com>
2026-04-14 15:44:23 +0200

    [44] Add a Gantt Diagram to the first workpackage of the Gantt Sample Template
    Issue: https://github.com/ObeoNetwork/pepper/issues/44

--- backend/pepper-starter/src/main/java/pepper/starter/services/PepperMMProjectTemplateInitializer.java
+++ backend/pepper-starter/src/main/java/pepper/starter/services/PepperMMProjectTemplateInitializer.java
@@ -12,19 +12,33 @@
  ******************************************************************************/
 package pepper.starter.services;
 
+import pepper.peppermm.Organization;
+import pepper.peppermm.Workpackage;
+
+import java.util.List;
 import java.util.UUID;
 
+import org.eclipse.sirius.components.collaborative.api.IRepresentationMetadataPersistenceService;
+import org.eclipse.sirius.components.collaborative.api.IRepresentationPersistenceService;
+import org.eclipse.sirius.components.collaborative.gantt.api.IGanttCreationService;
+import org.eclipse.sirius.components.core.RepresentationMetadata;
 import org.eclipse.sirius.components.core.api.IEditingContext;
 import org.eclipse.sirius.components.core.api.IEditingContextPersistenceService;
+import org.eclipse.sirius.components.core.api.IRepresentationDescriptionSearchService;
 import org.eclipse.sirius.components.emf.ResourceMetadataAdapter;
 import org.eclipse.sirius.components.emf.services.JSONResourceFactory;
 import org.eclipse.sirius.components.emf.services.api.IEMFEditingContext;
 import org.eclipse.sirius.components.events.ICause;
+import org.eclipse.sirius.components.gantt.Gantt;
+import org.eclipse.sirius.components.gantt.description.GanttDescription;
+import org.eclipse.sirius.components.representations.VariableManager;
+import org.eclipse.sirius.emfjson.resource.JsonResource;
 import org.eclipse.sirius.web.application.project.services.api.ISemanticDataInitializer;
 import org.springframework.stereotype.Service;
 
 import static pepper.starter.services.PepperMMProjectTemplateProvider.PEPPERMM_EMPTY;
 import static pepper.starter.services.PepperMMProjectTemplateProvider.PEPPERMM_PEPPER_SAMPLE;
+import static pepper.starter.services.view.ViewGanttDescriptionBuilder.WORKPACKAGE_GANTT_REP_DESC_NAME;
 
 /**
  * Provides Pepper meta model specific project templates initializers.
@@ -35,9 +49,19 @@ import static pepper.starter.services.PepperMMProjectTemplateProvider.PEPPERMM_P
 public class PepperMMProjectTemplateInitializer implements ISemanticDataInitializer {
 
     private final IEditingContextPersistenceService editingContextPersistenceService;
+    private final IRepresentationPersistenceService representationPersistenceService;
+    private final IGanttCreationService ganttCreationService;
+    private final IRepresentationDescriptionSearchService representationDescriptionSearchService;
+    private final IRepresentationMetadataPersistenceService representationMetadataPersistenceService;
 
-    public PepperMMProjectTemplateInitializer(IEditingContextPersistenceService editingContextPersistenceService) {
+    public PepperMMProjectTemplateInitializer(IEditingContextPersistenceService editingContextPersistenceService, IRepresentationPersistenceService representationPersistenceService,
+            IGanttCreationService ganttCreationService, IRepresentationDescriptionSearchService representationDescriptionSearchService,
+            IRepresentationMetadataPersistenceService representationMetadataPersistenceService) {
         this.editingContextPersistenceService = editingContextPersistenceService;
+        this.representationPersistenceService = representationPersistenceService;
+        this.ganttCreationService = ganttCreationService;
+        this.representationDescriptionSearchService = representationDescriptionSearchService;
+        this.representationMetadataPersistenceService = representationMetadataPersistenceService;
     }
 
     @Override
@@ -57,6 +81,8 @@ public class PepperMMProjectTemplateInitializer implements ISemanticDataInitiali
             resource.getContents().add(new PepperMMSampleBuilder().getSampleContent());
 
             this.editingContextPersistenceService.persist(cause, editingContext);
+
+            this.createGanttOfWorkpackage(cause, editingContext, resource);
         } else if (PepperMMProjectTemplateProvider.PEPPERMM_EMPTY_TEMPLATE_ID.equals(projectTemplateId) && editingContext instanceof IEMFEditingContext emfEditingContext) {
             var documentId = UUID.randomUUID();
             var resource = new JSONResourceFactory().createResourceFromPath(documentId.toString());
@@ -67,6 +93,34 @@ public class PepperMMProjectTemplateInitializer implements ISemanticDataInitiali
             resource.getContents().add(new PepperMMSampleBuilder().getEmptySampleContent());
 
             this.editingContextPersistenceService.persist(cause, editingContext);
+        }
+    }
+
+    private void createGanttOfWorkpackage(ICause cause, IEditingContext editingContext, JsonResource resource) {
+        var optionalGanttDescription = this.representationDescriptionSearchService.findAll(editingContext)
+                .values()
+                .stream()
+                .filter(GanttDescription.class::isInstance)
+                .map(GanttDescription.class::cast)
+                .filter(desc -> WORKPACKAGE_GANTT_REP_DESC_NAME.equals(desc.getLabel()))
+                .findFirst();
+        if (optionalGanttDescription.isPresent()) {
+            GanttDescription ganttDescription = optionalGanttDescription.get();
+            Workpackage workpackage = ((Organization) resource.getContents().get(0)).getOwnedProjects().get(0).getOwnedWorkpackages().get(0);
+            var variableManager = new VariableManager();
+            variableManager.put(VariableManager.SELF, workpackage);
+            String label = ganttDescription.labelProvider().apply(variableManager);
+            List<String> iconURLs = ganttDescription.getIconURLsProvider().apply(variableManager);
+
+            Gantt gantt = this.ganttCreationService.create(workpackage, ganttDescription, editingContext);
+            var representationMetadata = RepresentationMetadata.newRepresentationMetadata(gantt.getId())
+                    .kind(gantt.getKind())
+                    .label(label)
+                    .descriptionId(gantt.descriptionId())
+                    .iconURLs(iconURLs)
+                    .build();
+            this.representationMetadataPersistenceService.save(cause, editingContext, representationMetadata, gantt.targetObjectId());
+            this.representationPersistenceService.save(cause, editingContext, gantt);
         }
     }
 }
